7c21fbc0a49015e1c9a18cb8762ae3f313d596e7
atif9281 <atiff.khan92810000@gmail.com>
2025-02-26 14:24:33 +0500

    solved cors issue

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const assessmentRoutes = require('./routes/assessRoutes');
 const app = express();
 
 const corsOptions = {
-    origin: ["http://localhost:3000", "https://tafawoq-backend.onrender.com"],
+    origin: ["http://localhost:3000", "https://tafawoq-frontend-opal.vercel.app"],
     methods: "GET,POST,PUT,DELETE,OPTIONS,PATCH",
     allowedHeaders: "X-Requested-With, Content-Type, Authorization",
     credentials: true,
